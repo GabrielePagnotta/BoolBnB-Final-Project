85e4137c67b18c4cb8dadb2417111d89b045a853
AndreaPanzerDev <andpanz@libero.it>
2023-02-17 22:57:20 +0100

    comments

--- resources/views/admin/apartment/create_apartment.blade.php
+++ resources/views/admin/apartment/create_apartment.blade.php
@@ -9,60 +9,70 @@
 
         @csrf
 
+            {{-- Nome appartamento --}}
             <div class="mb-3">
               <label for="" class="form-label">Nome appartamento</label>
               <input type="text" class="form-control" id="" name="title">
             </div>
 
+            {{-- Numero stanze --}}
             <div class="mb-3">
               <label for="" class="form-label">Numero stanze</label>
               <input type="number" class="form-control" id="" name="rooms">
             </div>
 
+            {{-- Posti letto --}}
             <div class="mb-3">
                 <label for="" class="form-label">Posti letto</label>
                 <input type="number" class="form-control" id="" name="bedrooms">
             </div>
-
+            {{-- Numero bagni --}}
             <div class="mb-3">
                 <label for="" class="form-label">Numero bagni</label>
                 <input type="number" class="form-control" id="" name="bathrooms">
             </div>
 
+            {{-- Indirizzo --}}
             <div class="mb-3">
                 <label for="" class="form-label">Indirizzo</label>
                 <input type="text" class="form-control" id="" name="address">
             </div>
 
+            {{-- Metri quadri --}}
             <div class="mb-3">
                 <label for="" class="form-label">Metri quadri</label>
                 <input type="number" class="form-control" id="" name="square_meters">
             </div>
 
+            {{-- Prezzo --}}
             <div class="mb-3">
                 <label for="" class="form-label">Prezzo</label>
                 <input type="number" class="form-control" id="" name="price">
             </div>
 
-
+            {{-- immagine --}}
             <div class="mb-3">
                 <label for="" class="form-label">Immagine</label>
                 <input type="file" name="image">
             </div>
 
+            {{-- Descrizione --}}
             <div class="mb-3">
                 <label for="" class="form-label">Descrizione</label>
                 <textarea class="form-control"  name="description"></textarea>
             </div>
 
+            {{-- Visibilita' --}}
             <div class="mb-3 form-check">
                 <label class="form-check-label">Rendi visibile</label>
-                {{-- <input type="checkbox" name="visibility" value=""> --}}
 
+
+                {{-- <input type="checkbox" name="visibility" value=""> --}}
                 <input type="hidden" name="visibility" value="0">
                 <input type="checkbox" name="visibility" value="1">
             </div>
 
+            {{-- Servizi --}}
             <div class="mb-3">
                 <label for="">Servizi</label>
                 @foreach ($services as $service)
