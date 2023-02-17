301f7e4ff54867470cd70247309f86c52039daf8
Ale-1407 <alessiacarrera07@gmail.com>
2023-02-17 12:46:17 +0100

    form

--- resources/views/admin/apartment/create_apartment.blade.php
+++ resources/views/admin/apartment/create_apartment.blade.php
@@ -2,6 +2,65 @@
 
 @section('content')
     <div class="container">
-        <h1>ciao mamma</h1>
+        <form action="" method="POST">
+
+        @csrf
+            <div class="mb-3">
+              <label for="" class="form-label">Nome appartamento</label>
+              <input type="text" class="form-control" id="" aria-describedby="">
+            </div>
+
+            <div class="mb-3">
+              <label for="" class="form-label">Numero stanze</label>
+              <input type="number" class="form-control" id="">
+            </div>
+
+            <div class="mb-3">
+                <label for="" class="form-label">Posti letto</label>
+                <input type="number" class="form-control" id="">
+            </div>
+
+            <div class="mb-3">
+                <label for="" class="form-label">Numero bagni</label>
+                <input type="number" class="form-control" id="">
+            </div>
+
+            <div class="mb-3">
+                <label for="" class="form-label">Indirizzo</label>
+                <input type="text" class="form-control" id="">
+            </div>
+
+            <div class="mb-3">
+                <label for="" class="form-label">Metri quadri</label>
+                <input type="number" class="form-control" id="">
+            </div>
+
+            <div class="mb-3">
+                <label for="" class="form-label">Prezzo</label>
+                <input type="number" class="form-control" id="">
+            </div>
+
+            <div class="mb-3 form-check">
+              <input type="checkbox" class="form-check-input" id="exampleCheck1">
+              <label class="form-check-label" for="">Rendi visibile</label>
+            </div>
+
+            <div class="mb-3">
+                <label for="" class="form-label">Immagine</label>
+                <input type="file" name="image">
+            </div>
+
+            <div class="mb-3">
+                <label for="" class="form-label">Descrizione</label>
+                <textarea type="text" class="form-control" id="">
+            </div>
+
+            <div class="mb-3 form-check">
+                <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                <label class="form-check-label" for="">Rendi visibile</label>
+            </div>
+
+            <button type="submit" class="btn btn-primary">Submit</button>
+          </form>
     </div>
 @endsection
