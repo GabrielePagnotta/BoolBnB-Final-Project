df4558951591497157bf27cf96f22643875ac417
Ale-1407 <alessiacarrera07@gmail.com>
2023-02-20 18:53:54 +0100

    pic nav

--- resources/js/components/Navbar.vue
+++ resources/js/components/Navbar.vue
@@ -3,7 +3,7 @@
         <nav class="navbar navbar-expand-lg bg-body-tertiary" style="background-color: #e3f2fd;">
   <div class="container-fluid d-flex">
     <a href="/">
-        <img class="size-logo" src="https://www.ilfoglio.it/upload/1480336808148.png" alt="logo">
+        <img class="size-logo w-75" src="../assets/B.png" alt="logo">
     </a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
