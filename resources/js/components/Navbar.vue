cd9ace0dab2ef04709c93ce9da20c899f129b438
Yuri Di Domenico <113028513+yurididomenico@users.noreply.github.com>
2023-02-22 12:55:03 +0100

    Navbar Run watch fix

--- resources/js/components/Navbar.vue
+++ resources/js/components/Navbar.vue
@@ -4,7 +4,7 @@
             <div class="container-fluid d-flex">
                 <!-- left side -->
                 <a href="/">
-                    <img class="size-logo w-50" src="../../../public/images/B.png" alt="logo">
+                    <img class="size-logo w-50" src="../assets/B.png" alt="logo">
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                     aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
