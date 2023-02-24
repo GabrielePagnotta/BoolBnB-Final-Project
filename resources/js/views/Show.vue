a1bc3e51a0fe4723fb6fe70c5ee334d38e63a452
AndreaPanzerDev <andpanz@libero.it>
2023-02-24 18:04:51 +0100

    push e va cosi

--- resources/js/views/Show.vue
+++ resources/js/views/Show.vue
@@ -1,134 +1,167 @@
 <template>
     <div class="d-flex justify-content-center background">
+      <div class="card mb-3 card">
+        <img
+          v-if="ShowApartment.cover == null"
+          class="w-100"
+          src="https://cdn.open2b.com/5jwg8ozdvx/var/products/218/07/0-ac06c2c2-416-fornitura-di-proiettore-di-immagini-oleografiche.jpg"
+          alt="fff"
+        />
+        <img
+          v-else
+          class="w-100"
+          style="border-radius: 10px"
+          :src="`/storage/${ShowApartment.cover}`"
+          alt="apartment-image"
+        />
+        <div class="card-body">
+          <h2 class="card-title">{{ ShowApartment.title }}</h2>
+          <hr class="mb-2" />
 
-
-            <div class="card mb-3 card ">
-                <img v-if="ShowApartment.cover == null" class="w-100 "
-                src="https://cdn.open2b.com/5jwg8ozdvx/var/products/218/07/0-ac06c2c2-416-fornitura-di-proiettore-di-immagini-oleografiche.jpg"
-                alt="fff">
-                <img v-else class="w-100" style="border-radius: 10px;" :src="`/storage/${ShowApartment.cover}`" alt="apartment-image">
-                <div class="card-body">
-                    <h2 class="card-title">{{ ShowApartment.title }}</h2>
-                    <hr class="mb-2" >
-
-                    <div class="status justify-content-around my-4">
-                        <div class="area">
-                            <p class="mx-2"><strong>metri quadri: </strong> {{ShowApartment.square_meters}}</p>
-                            <hr>
-                            <p class="mx-2"><strong>Bagno/i:  </strong> {{ShowApartment.bathrooms}}</p>
-                            <hr>
-                            <p class="mx-2"><strong>Stanze/a: </strong> {{ShowApartment.rooms}}</p>
-                            <hr>
-                            <div>
-
-                                <p class="mx-2 btn btn-primary"><strong>Prezzo: </strong> {{ShowApartment.price}}€</p>
-                            </div>
-
-                        </div>
-                        <div class="area">
-
-                            <form action="" method="POST">
-                                <div v-if="authUser == null">
-                                    <input class="w-100 my-2" type="email" placeholder="inserisci email">
-                                </div>
-                                <div v-else>
-                                    <input class="w-100 my-2" type="email" :value=" authUser.email ">
-                                </div>
-
-
-                                <textarea class="w-100" name="" id="" cols="30" rows="10" placeholder="scrivi del testo..."></textarea>
-                                <button class="btn btn-primary">Invia</button>
-
-                            </form>
-                        </div>
-                    </div>
-                    <hr>
-                    <h4>
-                        Descrizione:
-                    </h4>
-                    <div class="d-flex justify-content-center">
-                        <p class="card-text">{{ShowApartment.description}}</p>
-                    </div>
-                </div>
+          <div class="status justify-content-around my-4">
+            <div class="area">
+              <p class="mx-2">
+                <strong>metri quadri: </strong> {{ ShowApartment.square_meters }}
+              </p>
+              <hr />
+              <p class="mx-2">
+                <strong>Bagno/i: </strong> {{ ShowApartment.bathrooms }}
+              </p>
+              <hr />
+              <p class="mx-2">
+                <strong>Stanze/a: </strong> {{ ShowApartment.rooms }}
+              </p>
+              <hr />
+              <div>
+                <p class="mx-2 btn btn-primary">
+                  <strong>Prezzo: </strong> {{ ShowApartment.price }}€
+                </p>
+              </div>
             </div>
+            <div class="area">
+              <form action="" method="POST">
+                <div v-if="authUser == null">
+                  <input
+                    class="w-100 my-2"
+                    type="email"
+                    placeholder="inserisci email"
+                  />
+                </div>
+                <div v-else>
+                  <input
+                    class="w-100 my-2"
+                    type="email"
+                    :value="authUser.email"
+                  />
+                </div>
 
+                <textarea
+                  class="w-100"
+                  name=""
+                  id=""
+                  cols="30"
+                  rows="10"
+                  placeholder="scrivi del testo..."
+                ></textarea>
+                <button class="btn btn-primary">Invia</button>
+              </form>
+            </div>
+          </div>
+          <hr />
+          <h4>Descrizione:</h4>
+          <div class="d-flex justify-content-center">
+            <p class="card-text">{{ ShowApartment.description }}</p>
+          </div>
+          <!--Mappa-->
+        <div id="map"></div>
+        </div>
 
-
-
-
-
-
-
+      </div>
     </div>
-</template>
 
-<script>
-import axios from 'axios'
+  </template>
 
-export default {
-    name: 'Show',
+  <script>
+  import axios from "axios";
+
+  export default {
+    name: "Show",
 
     data() {
-        return {
-            ShowApartment: [],
-            authUser:window.authUser,
-        }
+      return {
+        ShowApartment: [],
+
+        authUser: window.authUser,
+        center: [4, 4.44],
+      };
     },
     mounted() {
+      this.getShowApartments();
+      // console.log(this.ShowApartment);
 
+      console.log(this.center);
+      const map = tt.map({
+        key: "gfJDXxUVZKnn9kqVOkZ2tzc6DyGlkaWn",
+        container: "map",
+        center: this.center,
+        zoom: 10
+      })
 
-        this.getShowApartments();
+      map.on('load', () => {
+          new tt.Marker().setLngLat(this.center).addTo('map');
+      })
     },
 
     methods: {
-        getShowApartments() {
+      getShowApartments() {
+        axios
+          .get(
+            "http://127.0.0.1:8000/api/apartment/showed/" + this.$route.params.id
+          )
+          .then((response) => {
+            console.log(this.$route.params.id);
+            this.ShowApartment = response.data;
+            //this.center = [response.data.latitude, response.data.longitude];
+          });
+      },
+    },
+  };
+  </script>
 
-            axios.get("http://127.0.0.1:8000/api/apartment/showed/" + this.$route.params.id )
-                .then((response) => {
-                    console.log(this.$route.params.id)
-                    this.ShowApartment = response.data;
 
 
-                })
-
-        }
-    }
-}
-</script>
-
-<style lang="scss" scoped>
-
-.status{
+  <style lang="scss" scoped>
+  .status {
     display: flex;
-}
+  }
 
-.background{
-
+  .background {
     background-image: url(../assets/pattern.jpg);
-}
+  }
 
-.card{
+  .card {
     width: 50%;
-}
+  }
 
-.area{
+  .area {
     width: 50%;
-}
-
-@media screen and (max-width: 900px) {
-  .card{
-    width: 80%;
   }
 
-  .status{
-    display: block;
-  }
+  @media screen and (max-width: 900px) {
+    .card {
+      width: 80%;
+    }
 
-  .area{
-    width: 80%;
-    margin: auto;
+    .status {
+      display: block;
+    }
+
+    .area {
+      width: 80%;
+      margin: auto;
+    }
   }
-}
-</style>
+  </style>
+
 
 
--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -14,10 +14,17 @@
     <!-- Script Latitudine Longitudine -->
     <link rel="stylesheet" type="text/css"
         href="https://api.tomtom.com/maps-sdk-for-web/cdn/plugins/SearchBox/3.1.3-public-preview.0/SearchBox.css" />
-    <script
-        src="https://api.tomtom.com/maps-sdk-for-web/cdn/6.x/6.1.2-public-preview.15/services/services-web.min.js"></script>
-    <script
-        src="https://api.tomtom.com/maps-sdk-for-web/cdn/plugins/SearchBox/3.1.3-public-preview.0/SearchBox-web.js"></script>
+    <script src="https://api.tomtom.com/maps-sdk-for-web/cdn/6.x/6.1.2-public-preview.15/services/services-web.min.js">
+    </script>
+    <script src="https://api.tomtom.com/maps-sdk-for-web/cdn/plugins/SearchBox/3.1.3-public-preview.0/SearchBox-web.js">
+    </script>
+
+
+
+    <link rel="stylesheet" type="text/css"
+        href="https://api.tomtom.com/maps-sdk-for-web/cdn/6.x/6.23.0/maps/maps.css" />
+
+    <script src="https://api.tomtom.com/maps-sdk-for-web/cdn/6.x/6.23.0/maps/maps-web.min.js"></script>
 
     <title>Homepage</title>
 </head>
@@ -35,6 +42,7 @@
         @endif
     </div>
     <script src="{{ asset('js/app.js') }}"></script>
+    <script type="text/javascript"></script>
 </body>
 
 </html>
