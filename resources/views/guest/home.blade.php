2390498117ffad131cf50377c904dfd90c357e7f
Yuri Di Domenico <113028513+yurididomenico@users.noreply.github.com>
2023-02-24 16:31:48 +0100

    Link script TomTom in home.blade

--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -10,6 +10,15 @@
         integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
 
+
+    <!-- Script Latitudine Longitudine -->
+    <link rel="stylesheet" type="text/css"
+        href="https://api.tomtom.com/maps-sdk-for-web/cdn/plugins/SearchBox/3.1.3-public-preview.0/SearchBox.css" />
+    <script
+        src="https://api.tomtom.com/maps-sdk-for-web/cdn/6.x/6.1.2-public-preview.15/services/services-web.min.js"></script>
+    <script
+        src="https://api.tomtom.com/maps-sdk-for-web/cdn/plugins/SearchBox/3.1.3-public-preview.0/SearchBox-web.js"></script>
+
     <title>Homepage</title>
 </head>
 
