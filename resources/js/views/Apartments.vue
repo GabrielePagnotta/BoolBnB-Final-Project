164b81acc6877e3dbb063335986b4f42e2e1ee95
AndreaPanzerDev <andpanz@libero.it>
2023-02-25 11:39:37 +0100

    correzione

--- resources/js/views/Apartments.vue
+++ resources/js/views/Apartments.vue
@@ -1,6 +1,6 @@
 <template>
-    <div>
-        <!-- <div class="d-flex justify-content-center">
+  <div>
+    <!-- <div class="d-flex justify-content-center">
             <span @click="showApartments(false)" class="tag">tutti</span>
             <div v-for="service in Service" :key="service.id" @click="showApartments()">
                 <div>
@@ -12,77 +12,94 @@
             </div>
         </div> -->
 
-        <div>
-            <input type="hidden" name="latitude" id="inputLat">
-            <input type="hidden" name="longitude" id="inputLong">
+    <div>
+      <input type="hidden" name="latitude" id="inputLat" />
+      <input type="hidden" name="longitude" id="inputLong" />
 
       <!-- Searchbar Geo -->
       <div id="inputIndirizzo"></div>
     </div>
 
-        <!-- appartamenti -->
-        <div class="d-flex justify-content-center flex-wrap m-4">
-            <!-- Ciclo stampa appartamenti -->
-            <div v-for="apartment in Apartments" :key="apartment.id">
-                <div v-if="apartment.visibility == 1">
-                    <!-- Redirect Show singolo appartamento -->
-                    <router-link class="text" :to="`/showed/${apartment.id}`">
+    <!-- appartamenti -->
+    <div class="d-flex justify-content-center flex-wrap m-4">
+      <!-- Ciclo stampa appartamenti -->
+      <div v-for="apartment in Apartments" :key="apartment.id">
+        <div v-if="apartment.visibility == 1">
+          <!-- Redirect Show singolo appartamento -->
+          <router-link class="text" :to="`/showed/${apartment.id}`">
+            <!-- Carta -->
+            <div
+              id="card"
+              class="card mx-3 border"
+              style="max-width: 300px; height: 400px; border-radius: 20px"
+            >
+              <div
+                style="
+                  width: 100%;
+                  height: 300px;
+                  object-fit: cover;
+                  overflow: hidden;
+                "
+              >
+                <!-- Controllo immagine non trovata -->
+                <img
+                  v-if="apartment.cover == null"
+                  class="w-100"
+                  src="https://cdn.open2b.com/5jwg8ozdvx/var/products/218/07/0-ac06c2c2-416-fornitura-di-proiettore-di-immagini-oleografiche.jpg"
+                  alt="fff"
+                />
+                <img
+                  v-else
+                  class="w-100"
+                  style="border-radius: 10px"
+                  :src="`/storage/${apartment.cover}`"
+                  alt="apartment-image"
+                />
+              </div>
 
-                        <!-- Carta -->
-                        <div id="card" class="card mx-3 border"
-                            style="max-width: 300px; height: 400px; border-radius: 20px;">
-                            <div style="width: 100%; height: 300px; object-fit: cover; overflow: hidden;">
-                                <!-- Controllo immagine non trovata -->
-                                <img v-if="apartment.cover == null" class="w-100"
-                                    src="https://cdn.open2b.com/5jwg8ozdvx/var/products/218/07/0-ac06c2c2-416-fornitura-di-proiettore-di-immagini-oleografiche.jpg"
-                                    alt="fff">
-                                <img v-else class="w-100" style="border-radius: 10px;" :src="`/storage/${apartment.cover}`"
-                                    alt="apartment-image">
-                            </div>
+              <div class="card-body">
+                <h5 class="card-title">{{ apartment["title"] }}</h5>
+                <!-- Dettagli -->
+                <div class="d-flex justify-content-center">
+                  <div class="d-flex w-100 justify-content-around">
+                    <div>
+                      <i class="fa-solid fa-toilet px-2"></i>
+                      <span>{{ apartment["bathrooms"] }}</span>
+                    </div>
 
-                            <div class="card-body">
-                                <h5 class="card-title">{{ apartment['title'] }}</h5>
-                                <!-- Dettagli -->
-                                <div class="d-flex justify-content-center">
-                                    <div class="d-flex w-100  justify-content-around ">
-                                        <div>
-                                            <i class="fa-solid fa-toilet px-2"></i>
-                                            <span>{{ apartment['bathrooms'] }}</span>
-                                        </div>
+                    <div>
+                      <i class="fa-solid fa-bed"></i>
+                      <span>{{ apartment["bedrooms"] }}</span>
+                    </div>
+                  </div>
+                  <div class="d-flex w-100 justify-content-around">
+                    <div>
+                      <i class="fa-solid fa-door-closed"></i>
+                      <span>{{ apartment["rooms"] }}</span>
+                    </div>
 
-                                        <div>
-                                            <i class="fa-solid fa-bed"></i>
-                                            <span>{{ apartment['bedrooms'] }}</span>
-                                        </div>
-                                    </div>
-                                    <div class="d-flex w-100 justify-content-around">
-
-                                        <div>
-                                            <i class="fa-solid fa-door-closed"></i>
-                                            <span>{{ apartment['rooms'] }}</span>
-                                        </div>
-
-                                        <div>
-                                            <i class="fa-solid fa-ruler-combined"></i>
-                                            <span>{{ apartment['square_meters'] }}</span>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div>
-                                    <!-- Prezzo -->
-                                    <p class="text-center my-3"><strong>Prezzo: </strong>{{ apartment['price'] }}€</p>
-                                    <p class="text-center my-3"><strong>Località: </strong>{{ apartment['address'] }}</p>
-                                </div>
-                            </div>
-                        </div>
-
-                    </router-link>
+                    <div>
+                      <i class="fa-solid fa-ruler-combined"></i>
+                      <span>{{ apartment["square_meters"] }}</span>
+                    </div>
+                  </div>
                 </div>
-
-
+                <div>
+                  <!-- Prezzo -->
+                  <p class="text-center my-3">
+                    <strong>Prezzo: </strong>{{ apartment["price"] }}€
+                  </p>
+                  <p class="text-center my-3">
+                    <strong>Località: </strong>{{ apartment["address"] }}
+                  </p>
+                </div>
+              </div>
             </div>
+          </router-link>
         </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
@@ -215,38 +232,37 @@ export default {
         }
     }
 }
+}}
+
 
 </script>
 
 
-
-
 <style lang="scss" scoped>
 .card:hover {
-    transform: scale(1.1);
-
+  transform: scale(1.1);
 }
 
 .text {
-    text-decoration: none;
-    color: black;
+  text-decoration: none;
+  color: black;
 }
 
 .card {
-    transition: transform 500ms;
+  transition: transform 500ms;
 }
 
 .tag {
-    font-size: 1.3rem;
-    background-color: pink;
-    border-radius: 10px;
-    padding: 10px;
-    margin: 1rem;
-    color: white;
-    cursor: pointer;
+  font-size: 1.3rem;
+  background-color: pink;
+  border-radius: 10px;
+  padding: 10px;
+  margin: 1rem;
+  color: white;
+  cursor: pointer;
 }
 
 .tag:hover {
-    background-color: crimson;
+  background-color: crimson;
 }
 </style>
