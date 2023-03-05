f43472e24476419e196d680c0f11e8824e25ddbf
Gabriele Pagnotta <gabriele.pagnotta1998@gmail.com>
2023-03-05 21:40:18 +0100

    creazione footer

---
+++ resources/js/components/Footer.vue
@@ -0,0 +1,80 @@
+<template>
+    <div class="container-fluid d-flex justify-content-center footercontainer">
+        <div class="text-center">
+            <p>Developed by:</p>
+            <div class="d-flex flex-wrap justify-content-center">
+                <div>
+                    <div class="mx-2 yuri"></div>
+                    <p class="bold my-3">Yuri Di Domenico</p>
+                </div>
+                <div>
+                    <div class="mx-2 ale"></div>
+                    <p class="bold my-3">Alessia Carrera</p>
+                </div>
+                <div>
+                    <div class="mx-2 yuri"></div>
+                    <p class="bold my-3">Andrea Panzeri</p>
+                </div>
+                <div>
+                    <div class="mx-2 gab"></div>
+                    <p class="bold my-3">Gabriele Pagnotta</p>
+                </div>
+
+
+        </div>
+
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'Footer'
+    }
+</script>
+
+<style lang="scss" scoped>
+    .footercontainer{
+        background-color: #FF3E61;
+        color:white;
+    }
+
+
+
+    .yuri{
+        background-image: url(../assets/yuri.png);
+        width:200px;
+        height: 200px;
+        background-color: white;
+        background-size:76%;
+        background-repeat: no-repeat;
+        background-position: center;
+        border-radius: 100px 100px 100px 100px;
+    }
+
+    .ale{
+        background-image: url(../assets/alessia.jpg);
+        width:200px;
+        height: 200px;
+        background-color: white;
+        background-size:95%;
+        background-repeat: no-repeat;
+        background-position: center;
+        border-radius: 100px 100px 100px 100px;
+    }
+    .gab{
+        background-image: url(../assets/gab.png);
+        width:200px;
+        height: 200px;
+        background-color: white;
+        background-size:91%;
+        background-repeat: no-repeat;
+        background-position: center;
+        border-radius: 100px 100px 100px 100px;
+    }
+
+
+    .bold{
+        font-weight: bold;
+    }
+</style>
--- resources/js/views/App.vue
+++ resources/js/views/App.vue
@@ -3,17 +3,18 @@
         <Navbar />
         <!-- <Apartments :info="Apartments"/> -->
         <router-view></router-view>
+        <Footer/>
     </div>
 </template>
 
 <script>
     import Navbar from '../components/Navbar.vue'
-    // import Apartments from '../views/Apartments.vue'
+    import Footer from '../components/Footer.vue'
 
     export default {
     name: "app",
     components:{
-        Navbar,
+        Navbar,Footer
     }
 
 }
